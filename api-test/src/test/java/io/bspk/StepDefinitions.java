b2ef01491042081e9b5c6c8e798f729dc78b261a
Peter Anning <22272449+peteanning@users.noreply.github.com>
2022-05-12 13:23:08 +0100

    implemented a basic feature

--- api-test/src/test/java/io/bspk/StepDefinitions.java
+++ api-test/src/test/java/io/bspk/StepDefinitions.java
@@ -1,6 +1,22 @@
 package io.bspk;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class StepDefinitions {
 
+    @Given("I have {int} cukes in my belly")
+    public void i_have_n_cukes_in_my_belly(int cukes) {
+        System.out.format("Cukes: %n\n", cukes);
+    }
+
+    @When("I wait 1 hour")
+    public void when_i_wait () {
+        System.out.println("I have waited one hour");
+    }
+
+    @Then("my belly should growl")
+    public void my_belly_should_growl () {
+        System.out.println("my belly should growl");
+    }
 }
